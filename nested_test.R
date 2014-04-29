f38799cc8fbf8d2eec6fff4f54528668b745e102
Luis Zaman <zaman@luiss-air.gateway.2wire.net>
2014-04-29 01:19:23 -0400

    updated test again

--- nested_test.R
+++ nested_test.R
@@ -1,8 +1,10 @@
 require(Rcpp)
+setwd('~/RcppNested')
 
 raw_data <- read.csv('interaction_matrix.csv', head=F)
 csv_mat <- as.matrix(raw_data)
 
 sourceCpp("nestedness.cpp")
-image(nested_matrix(csv_mat))
-image(something(csv_mat))
+
+vegan::nestednodf(csv_mat)
+calculateNODF(csv_mat)
